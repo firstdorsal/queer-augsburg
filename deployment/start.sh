5cee17586013060671f1eb7d76775b227e5f5403
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@vindelicum.eu>
2023-12-09 21:48:31 +0100

    add basic mattermost deployment

--- deployment/start.sh
+++ deployment/start.sh
@@ -19,7 +19,8 @@ mozart render templates/admin.yml -o results/admin.yml
 cp -r templates/email-templates results/
 cp -r templates/data results/
 find results/data -type f -exec chmod 0644 {} \;
-
+#mkdir -p results/config/mattermost
+#chmod 0777 results/config/mattermost/
 
 
 docker compose -p qa --project-directory results/ up --build -d --remove-orphans 
