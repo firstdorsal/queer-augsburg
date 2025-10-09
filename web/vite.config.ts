f13702c81129e5ba2588fd1a7adb9cb1252444cc
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@vindelicum.eu>
2025-10-10 00:28:20 +0200

    search with button instead of instant

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,6 +7,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        minify: "terser"
+    },
     plugins: [
         preact(),
         imagetools(),
