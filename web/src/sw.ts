b2278c86babcdfc6c692670afb38ae310b8536a8
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@vindelicum.eu>
2025-10-09 20:11:31 +0200

    cache get_meetings

--- web/src/sw.ts
+++ web/src/sw.ts
@@ -1,9 +1,28 @@
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { NetworkOnly } from "workbox-strategies";
+import { NetworkFirst, NetworkOnly } from "workbox-strategies";
 
 declare let self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(({ url }) => url.pathname.startsWith("/static/"), new NetworkOnly());
+
+// Cache the API endpoint with offline fallback
+registerRoute(
+    ({ url }) => url.href === "https://api.queer-augsburg.de/api/get_meetings/?i=0&t=Active",
+    new NetworkFirst({
+        cacheName: "api-cache",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200] // Cache successful responses
+            }),
+            new ExpirationPlugin({
+                maxEntries: 50,
+                maxAgeSeconds: 24 * 60 * 60 // 24 hours
+            })
+        ]
+    })
+);
