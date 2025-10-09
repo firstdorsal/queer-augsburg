7907e8d9bb900a69ba760758f1a72225ce920c0c
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@vindelicum.eu>
2025-10-09 20:16:53 +0200

    update caching

--- web/src/sw.ts
+++ web/src/sw.ts
@@ -12,16 +12,18 @@ registerRoute(({ url }) => url.pathname.startsWith("/static/"), new NetworkOnly(
 
 // Cache the API endpoint with offline fallback
 registerRoute(
-    ({ url }) => url.href === "https://api.queer-augsburg.de/api/get_meetings/?i=0&t=Active",
+    ({ url }) =>
+        url.origin === "https://api.queer-augsburg.de" &&
+        url.pathname === "/api/get_meetings/?t=Active",
     new NetworkFirst({
         cacheName: "api-cache",
         plugins: [
             new CacheableResponsePlugin({
-                statuses: [0, 200] // Cache successful responses
+                statuses: [0, 200]
             }),
             new ExpirationPlugin({
-                maxEntries: 50,
-                maxAgeSeconds: 24 * 60 * 60 // 24 hours
+                maxEntries: 20, // Cache multiple variations of query params
+                maxAgeSeconds: 7 * 24 * 60 * 60 // 1 week
             })
         ]
     })
